ba4b303c23d75799b29e6dbb8aa728ec1b00c872
Seykonag <150951069+Seykonag@users.noreply.github.com>
2024-02-10 22:25:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -31,36 +31,38 @@ var root = document.querySelector(':root');
  * Roll one reel
  */
 const roll = (reel, offset = 0) => {
-  // Minimum of 2 + the reel offset rounds
-  const delta = (offset + 2) * num_icons + Math.round(Math.random() * num_icons);
-
-  // Return promise so we can wait for all reels to finish
-  return new Promise((resolve, reject) => {
-    const style = getComputedStyle(reel),
-      // Current background position
-      backgroundPositionY = parseFloat(style["background-position-y"]),
-      // Target background position
-      targetBackgroundPositionY = backgroundPositionY + delta * icon_height,
-      // Normalized background position, for reset
-      normTargetBackgroundPositionY = targetBackgroundPositionY % (num_icons * icon_height);
-
-    // Delay animation with timeout, for some reason a delay in the animation property causes stutter
-    setTimeout(() => {
-      // Set transition properties ==> https://cubic-bezier.com/#.41,-0.01,.63,1.09
-      reel.style.transition = `background-position-y ${(8 + 1 * delta) * time_per_icon}ms cubic-bezier(.41,-0.01,.63,1.09)`;
-      // Set background position
-      reel.style.backgroundPositionY = `${backgroundPositionY + delta * icon_height}px`;
-    }, offset * 150);
-
-    // After animation
-    setTimeout(() => {
-      // Reset position, so that it doesn't get higher without limit
-      reel.style.transition = `none`;
-      reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
-      // Resolve this promise
-      resolve(delta % num_icons);
-    }, (8 + 1 * delta) * time_per_icon + offset * 150);
-  });
+	// Minimum of 2 + the reel offset rounds
+	const delta = (offset + 2) * num_icons + Math.round(Math.random() * num_icons); 
+	
+	// Return promise so we can wait for all reels to finish
+	return new Promise((resolve, reject) => {
+		
+		const style = getComputedStyle(reel),
+					// Current background position
+					backgroundPositionY = parseFloat(style["background-position-y"]),
+					// Target background position
+					targetBackgroundPositionY = backgroundPositionY + delta * icon_height,
+					// Normalized background position, for reset
+					normTargetBackgroundPositionY = targetBackgroundPositionY%(num_icons * icon_height);
+		
+		// Delay animation with timeout, for some reason a delay in the animation property causes stutter
+		setTimeout(() => { 
+			// Set transition properties ==> https://cubic-bezier.com/#.41,-0.01,.63,1.09
+			reel.style.transition = `background-position-y ${(8 + 1 * delta) * time_per_icon}ms cubic-bezier(.41,-0.01,.63,1.09)`;
+			// Set background position
+			reel.style.backgroundPositionY = `${backgroundPositionY + delta * icon_height}px`;
+		}, offset * 150);
+			
+		// After animation
+		setTimeout(() => {
+			// Reset position, so that it doesn't get higher without limit
+			reel.style.transition = `none`;
+			reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
+			// Resolve this promise
+			resolve(delta%num_icons);
+		}, (8 + 1 * delta) * time_per_icon + offset * 150);
+		
+	});
 };
 
 /**
@@ -84,9 +86,6 @@ function rollAll() {
   debugEl.textContent = 'rolling...';
   const reelsList = document.querySelectorAll('.slots > .reel');
 
-  // Сброс значений indexes перед новым запуском анимации
-  indexes.fill(0);
-
   Promise.all([...reelsList].map((reel, i) => roll(reel, i)))
     .then(deltas => {
       // add up indexes
@@ -94,22 +93,25 @@ function rollAll() {
       debugEl.textContent = indexes.map(i => iconMap[i]).join(' - ');
 
       // Win conditions
-      if (indexes[0] == indexes[1] || indexes[1] == indexes[2]) {
-        const winCls = indexes[0] == indexes[2] ? "win2" : "win1";
+      if (indexes[0] == indexes[1] && indexes[1] == indexes[2]) {
+        const winCls = "win2";
         document.querySelector(".slots").classList.add(winCls);
 
         // Добавить обработчик события transitionend
         document.querySelector(".slots").addEventListener('transitionend', handleAnimationEnd);
 
-        setTimeout(() => {
-          document.querySelector(".slots").classList.remove(winCls);
-          // Again!
-          rollAll();
-        }, 2000);
-      } else {
-        // Again!
-        handleAnimationEnd();
+        setTimeout(() => document.querySelector(".slots").classList.remove(winCls), 2500);
+      } else if (indexes[0] == indexes[1] || indexes[1] == indexes[2]) {
+        const winCls = "win1";
+        document.querySelector(".slots").classList.add(winCls);
+
+        // Добавить обработчик события transitionend
+        document.querySelector(".slots").addEventListener('transitionend', handleAnimationEnd);
+
+        setTimeout(() => document.querySelector(".slots").classList.remove(winCls), 2500);
       }
+      // Again!
+      handleAnimationEnd();
     });
 }
 
@@ -131,26 +133,26 @@ leverBall.addEventListener('click', function () {
 });
 
 function getRequest() {
-// Это все запрос на баланс
-let request = new XMLHttpRequest();
+  // Это все запрос на баланс
+  let request = new XMLHttpRequest();
 
-request.onload = () => {
-  if (request.status === 200) {
-    const json = request.response;
-    document.getElementById("balance").innerHTML = json["balance"];
+  request.onload = () => {
+    if (request.status === 200) {
+      const json = request.response;
+      document.getElementById("balance").innerHTML = json["balance"];
+    }
+  };
+
+  request.responseType = "json";
+  request.open("GET", "https://chupa-pupa-29ab2bbfb5f8.herokuapp.com/balanceValue/" + String(idUser));
+  request.setRequestHeader("Accept", "application/json");
+  request.send();
   }
-};
-
-request.responseType = "json";
-request.open("GET", "https://chupa-pupa-29ab2bbfb5f8.herokuapp.com/balanceValue/" + String(idUser));
-request.setRequestHeader("Accept", "application/json");
-request.send();
-}
-// А это некий предзапрос, он принимает value(сумму выигрыша, проигрыша), а также
-// от себя добавляет id пользователя телеграмма, на данный момент он отправляет
-// только мой id для теста смотри html, button test
-function inPost(value) {
-  postReq(String(idUser), Number(value));
+  // А это некий предзапрос, он принимает value(сумму выигрыша, проигрыша), а также
+  // от себя добавляет id пользователя телеграмма, на данный момент он отправляет
+  // только мой id для теста смотри html, button test
+  function inPost(value) {
+    postReq(String(idUser), Number(value));
 }
 
 //А это запрос на изменение
