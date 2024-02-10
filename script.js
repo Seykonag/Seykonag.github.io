fa6fd00425b4cdb8b7f481da18c0341d9bd06827
Seykonag <150951069+Seykonag@users.noreply.github.com>
2024-02-11 00:12:56 +0600

    Update script.js

--- script.js
+++ script.js
@@ -165,13 +165,15 @@ $(document).ready(function() {
     count = count < 1 ? 1 : count;
     $input.val(count);
 });
+
 // Прибавляем кол-во по клику
 $('.quantity_inner .bt_plus').click(function() {
     let $input = $(this).parent().find('.quantity');
     let count = parseInt($input.val()) + 5;
     count = count > parseInt($input.data('max-count')) ? parseInt($input.data('max-count')) : count;
     $input.val(parseInt(count));
-}); 
+});
+	
 // Убираем все лишнее и невозможное при изменении поля
 $('.quantity_inner .quantity').bind("change keyup input click", function() {
     if (this.value.match(/[^0-9]/g)) {
