0d96257185e1964b38df71481309ba21f8a709af
romantos909 <156901161+romantos909@users.noreply.github.com>
2024-02-10 23:56:05 +0600

    Update script.js jquery joska

--- script.js
+++ script.js
@@ -163,3 +163,30 @@ function postReq(userId, value) {
   secondRequest.setRequestHeader("Accept", "application/json");
   secondRequest.send(body);
 }
+
+// Убавляем кол-во по клику
+    $('.quantity_inner .bt_minus').click(function() {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) - 5;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+});
+// Прибавляем кол-во по клику
+$('.quantity_inner .bt_plus').click(function() {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) + 5;
+    count = count > parseInt($input.data('max-count')) ? parseInt($input.data('max-count')) : count;
+    $input.val(parseInt(count));
+}); 
+// Убираем все лишнее и невозможное при изменении поля
+$('.quantity_inner .quantity').bind("change keyup input click", function() {
+    if (this.value.match(/[^0-9]/g)) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    }
+    if (this.value == "") {
+        this.value = 5;
+    }
+    if (this.value > parseInt($(this).data('max-count'))) {
+        this.value = parseInt($(this).data('max-count'));
+    }    
+});    
